4e0e115c8fc1f2dd20683ecae109bd0d746aa394
jouad01 <jouardiouardi@cifpfbmoll.eu>
2023-02-08 18:59:44 +0100

    Initial commit

---
+++ src/test/java/edu/daw/AppTest.java
@@ -0,0 +1,12 @@
+package edu.daw;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}
